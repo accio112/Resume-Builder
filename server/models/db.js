23bed0b33018aa44cd98e72853770e1b1375f655
accio112 <divyaatnet2015@gmail.com>
2019-03-10 01:02:55 +0530

    minor errors fixed

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (!err) { console.log('MongoDB connection succeeded.'); }
-    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
-});
+mongoose.connect('mongodb://dheeraj:dheeraj123@ds117681.mlab.com:17681/resume',
+    {useNewUrlParser: true }, (err) => {
+        if (!err) { console.log('MongoDB connection succeeded.'); }
+        else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+    });
 
 require('./user.model');
